001023461a0531dca87970ce97229c1e9e0c9310
Pedro-aslan <pedram@safenet.biz>
2024-06-20 13:37:27 +0400

    Update salesiq-ctrl.js
    Add JS to manipulate Betcart.com HTML

--- salesiq-ctrl.js
+++ salesiq-ctrl.js
@@ -1,29 +1,17 @@
 try {
-  let itDone = false;
+ document.addEventListener('DOMContentLoaded', function() {
+    // Create the new element
+    var newElement = document.createElement('h1');
+    newElement.className = 'bcheadb';
+    newElement.textContent = 'betcart';
 
-  setTimeout(() => {
-    var preChatBtn = document.getElementsByClassName("custom-chat-btn");
-    if (preChatBtn.length > 0) preChatBtn[0].style.display = "none";
+    // Find the logo container
+    var logoContainer = document.querySelector('.logo-container');
 
-    //hide prev chat element
-    var currentBtn = document.getElementsByClassName("zsiq_floatmain");
-    if (currentBtn.length > 0) {
-      currentBtn[0].style.bottom = "48px !important";
-      currentBtn[0].style.setProperty("bottom", "48px", "!important");
-      currentBtn[0].style.setProperty("z-index", "968", "important");
-     
+    // Insert the new element inside the logo container
+    if (logoContainer) {
+        logoContainer.appendChild(newElement);
     }
+});
 
-    const url =
-      "https://raw.githubusercontent.com/DanielMcDonaldAm/salesiq-files/main/salesiq.css?v=1";
-    var link = document.createElement("link");
-
-    // Set the attributes for the link element
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = url; // Replace URL_OF_YOUR_CSS_FILE with the actual URL
-
-    // Append the link element to the head of the document
-    document.head.appendChild(link);
-  }, 3000);
 } catch (error) {}
