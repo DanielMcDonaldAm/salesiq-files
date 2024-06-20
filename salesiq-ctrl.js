2c7872f02a30e4ef2570e8637916b438832c712f
Pedro-aslan <pedram@safenet.biz>
2024-06-20 13:42:32 +0400

    Update salesiq-ctrl.js

--- salesiq-ctrl.js
+++ salesiq-ctrl.js
@@ -1,17 +1,26 @@
 try {
- document.addEventListener('DOMContentLoaded', function() {
-    // Create the new element
-    var newElement = document.createElement('h1');
-    newElement.className = 'bcheadb';
-    newElement.textContent = 'betcart';
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
 
-    // Find the logo container
+    // Check if we can select the logo container
     var logoContainer = document.querySelector('.logo-container');
 
-    // Insert the new element inside the logo container
     if (logoContainer) {
+        console.log('Logo container found');
+
+        // Create the new element
+        var newElement = document.createElement('h1');
+        newElement.className = 'bcheadb';
+        newElement.textContent = 'betcart';
+
+        // Insert the new element inside the logo container
         logoContainer.appendChild(newElement);
+
+        console.log('New element added');
+    } else {
+        console.log('Logo container not found');
     }
 });
+
 
 } catch (error) {}
